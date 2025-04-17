2f398b709f7b51e63e363c0a4b52020464962fa7
vilar <loadslowly@gmail.com>
2025-04-17 15:56:28 +0500

    Initial project setup with Spring Boot skeleton

---
+++ src/test/java/ru/effectivemobile/taskmanagement/TaskmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effectivemobile.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
