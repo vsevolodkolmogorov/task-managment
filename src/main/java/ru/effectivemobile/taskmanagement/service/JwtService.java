da225bdc4b0bd0c2fadbc3b49906e4d1d6e3f86e
vilar <loadslowly@gmail.com>
2025-04-19 00:36:23 +0500

    feat: added Controller and Services for work with JWT and User
    Related to: #3

---
+++ src/main/java/ru/effectivemobile/taskmanagement/service/JwtService.java
@@ -0,0 +1,9 @@
+package ru.effectivemobile.taskmanagement.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+    String generateToken(UserDetails userDetails);
+    String extractUsername(String token);
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
