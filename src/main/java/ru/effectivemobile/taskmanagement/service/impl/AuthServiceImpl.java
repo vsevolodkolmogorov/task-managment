e1f959e96024aa9a152817a457026eb4f9bad747
vilar <loadslowly@gmail.com>
2025-05-19 20:04:15 +0500

    fix: delete repeat code ( bag from pull request )

--- src/main/java/ru/effectivemobile/taskmanagement/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/effectivemobile/taskmanagement/service/impl/AuthServiceImpl.java
@@ -55,8 +55,6 @@ public class AuthServiceImpl implements AuthService {
                 .password(passwordEncoder.encode(request.getPassword()))  // Password encryption
                 .role(roleRepository.findByCode(RoleCode.USER.name())
                         .orElseThrow(() -> new IllegalStateException("Role USER not found in DB")))
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
                 .build();
         userRepository.save(user);
 
